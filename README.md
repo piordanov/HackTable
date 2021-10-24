6ed7add7eef16e5c2f4999dc8ca58a57f8db0066
Peter Iordanov <peter@bulbyte.com>
2021-10-24 21:05:32 +0100

    generate simplified heat map

---
+++ README.md
@@ -0,0 +1,13 @@
+# HackTable
+
+This server acts a proof of concept of displaying a commit heat map that updates while teams work on their projects.
+
+## Setup
+
+Clone the project onto a raspberry pi with a SenseHat (follow instructions for setup found in the (Hacking the Hack Guide)[https://github.com/augustluhrs/HackTheHack_RaspiGuide]
+
+Run `npm install` to setup the webserver.
+
+Also, will need to set up a localtunnel, this guide can assist.
+
+You will aslo need to setup a Github webhook (currently this is connected to this project's webhook, but that can be changed to any url provided by your Raspberry Pi.
--- webServer.js
+++ webServer.js
@@ -3,56 +3,61 @@
 const express = require('express');
 const { createServer } = require('http');
 // const WebSocket = require('ws');
-// 
+// node sensehat led library
+const sense = require("sense-hat-led").sync;
+const axios = require("axios");
+const { DateTime } = require("luxon");
+
 const server = createServer(function (req, res) {
 	handler(req, res, function (err) {
 		res.statusCode = 404;
 		res.end('no such location')
 	});
+	
 });
-// const wss = new WebSocket.Server({ server });
-
-// tell the server where our webpage files are
-// app.use(express.static('public'));
-
-// node sensehat led library
-const sense = require("sense-hat-led").sync;
 
 //default image to display on start
 var X = [0, 255, 0];  // Green
-var O = [255, 255, 255];  // White
-var leds = [
+var O = [0 , 0, 0];  // Black
+const cleanLeds = [
 O, O, O, O, O, O, O, O,
-O, X, X, O, O, X, X, O,
-O, X, X, O, O, X, X, O,
 O, O, O, O, O, O, O, O,
-O, X, O, O, O, O, X, O,
-O, X, O, O, O, O, X, O,
-O, X, X, X, X, X, X, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O
 ];
 
-// sense.setPixels(leds);
-
-function buildHeatMap() {
+async function buildHeatMap() {
 	// fetch commits, build heatmap like leds above
+	const response = await axios.get('https://api.github.com/repos/piordanov/HackTable/commits');
+	let commits = response.data;
+	let leds = cleanLeds.slice();
+	
+	let commit_times = commits.map(c => DateTime.fromISO(c.commit.author.date));
+	let firstTime = commit_times[0];
+	for (let idx = 1; idx < commit_times.length; idx++) {
+		let curr = commit_times[idx];
+		let duration = firstTime.diff(curr, ["hours"])
+		let index = Math.round(duration.hours);
+		if (index < 64) {
+			leds[index] = X;
+		}
+	}
 	sense.setPixels(leds);
+
 }
 
 const createHandler = require('github-webhook-handler')
-const handler = createHandler({ path: '/payload', secret: 'mysecret'});
-
-//app.post('/payload', function (req, res) {
-//	console.log(req.body);
-//	
-//	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
-//	buildHeatMap();
-//});
+const handler = createHandler({ path: '/payload', secret: 'myhashsecret'});
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.pusher.name)
+  	console.log('Received a push event for %s by %s', 
+		event.payload.repository.name,
+		event.payload.pusher.name);
+	buildHeatMap();
 });
 
 //set up the server, listening on port 8080
